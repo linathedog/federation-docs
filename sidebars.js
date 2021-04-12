464f72f96faabca0b336198937d20e8fa7e3731e
Andreas <koza-sparrow@hotmail.com>
2021-04-12 13:04:28 +0300

    Fixed boken image links, sidebar links

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    'User Manual': ['introduction','user_roles', 'login','service_list','invitations','deployment','doc1'],
+    'User Manual': ['introduction','user_roles', 'login','service_list','invitations','deployment'],
     'Testing Guidelines': ['end_users','admins'],
   },
 };
--- src/components/components.js
+++ src/components/components.js
@@ -1,5 +1,19 @@
 import React,{useState,useEffect} from 'react';
 import Collapse from 'react-bootstrap/Collapse';
+import ReactPlayer from 'react-player'
+
+// Render a YouTube video player
+export const VideoPlayer = ({children,video_url}) => {
+  const [vidSrc,setVidSrc] = useState('../static/vid'+ video_url);
+  const loadBuild = () => {
+    setVidSrc('../vid'+ video_url);
+  }
+    return (
+      <ReactPlayer url={video_url}
+       />
+    )
+}
+
 
 export const ImageExpand = ({children,image_url}) => {
     const [show,setShow]= useState(false);
@@ -9,6 +23,7 @@ export const ImageExpand = ({children,image_url}) => {
     }
     return (
       <React.Fragment>
+
         <div style={{display:"inline-block"}} onClick={()=>setShow(!show)}>
           <a className='custom-link'>{show?'Hide Image':'View Image'}</a>
         </div>
