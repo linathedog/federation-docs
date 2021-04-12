eef6091642fe36c0d1b57630aa1d990255cec0be
Andreas <koza-sparrow@hotmail.com>
2021-04-12 13:11:55 +0300

    removed package that was not used

--- src/components/components.js
+++ src/components/components.js
@@ -1,18 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import Collapse from 'react-bootstrap/Collapse';
-import ReactPlayer from 'react-player'
 
-// Render a YouTube video player
-export const VideoPlayer = ({children,video_url}) => {
-  const [vidSrc,setVidSrc] = useState('../static/vid'+ video_url);
-  const loadBuild = () => {
-    setVidSrc('../vid'+ video_url);
-  }
-    return (
-      <ReactPlayer url={video_url}
-       />
-    )
-}
 
 
 export const ImageExpand = ({children,image_url}) => {
